0ded88d481ef4e8e05e41fc28bd7241cd349bfc6
Saranraj Thangaraj <45966953+Saran1296@users.noreply.github.com>
2025-04-12 21:05:00 +0530

    Update HelloWorldTest.java

--- src/main/java/hello/HelloWorldTest.java
+++ src/main/java/hello/HelloWorldTest.java
@@ -1,13 +1,13 @@
-package hello;
+// package hello;
 
-import org.junit.Test;  // JUnit 4 annotation
-import static org.junit.Assert.assertEquals;  // JUnit 4 assertion
+// import org.junit.Test;  // JUnit 4 annotation
+// import static org.junit.Assert.assertEquals;  // JUnit 4 assertion
 
-public class HelloWorldTest {
+// public class HelloWorldTest {
 
-    @Test
-    public void testSayHello() {
-        HelloWorld helloWorld = new HelloWorld();
-        assertEquals("Hello world", helloWorld.sayHello());
-    }
-}
+//     @Test
+//     public void testSayHello() {
+//         HelloWorld helloWorld = new HelloWorld();
+//         assertEquals("Hello world", helloWorld.sayHello());
+//     }
+// }
