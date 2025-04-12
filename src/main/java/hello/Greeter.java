34c4a0056eba53393c424d6bc61972244c5d7cc4
Saranraj Thangaraj <45966953+Saran1296@users.noreply.github.com>
2025-04-12 19:27:26 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "Intoduction to DevOps Demo";
+        return "Intoduction to DevOps Demo test poll scm";
     }
 }
