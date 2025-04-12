c6f7bd85e7b2fef739b3c90093db7c736927cf88
Saranraj Thangaraj <45966953+Saran1296@users.noreply.github.com>
2025-04-12 20:34:12 +0530

    Added test cases

---
+++ src/main/java/hello/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testSayHello() {
+        HelloWorld helloWorld = new HelloWorld();
+        String result = helloWorld.sayHello();
+        assertEquals("Hello world", result);
+    }
+}
