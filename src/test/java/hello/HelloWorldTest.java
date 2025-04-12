7abbb86fa331201553e9c2b58c2e2f8a557f77e0
Saranraj Thangaraj <45966953+Saran1296@users.noreply.github.com>
2025-04-12 21:02:59 +0530

    Add files via upload

---
+++ src/test/java/hello/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.junit.Test;  // JUnit 4 annotation
+import static org.junit.Assert.assertEquals;  // JUnit 4 assertion
+
+public class HelloWorldTest {
+
+    @Test
+    public void testSayHello() {
+        HelloWorld helloWorld = new HelloWorld();
+        assertEquals("Hello world", helloWorld.sayHello());
+    }
+}
